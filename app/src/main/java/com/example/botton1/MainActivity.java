85ae8d918a2c6e9861de1897a79d370b69ec7b5e
imageview2 <ba6677@bs.amalnet.k12.il>
2024-01-23 22:27:45 +0200

    ee

--- app/src/main/java/com/example/botton1/MainActivity.java
+++ app/src/main/java/com/example/botton1/MainActivity.java
@@ -2,21 +2,27 @@ package com.example.botton1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
+    TextView tv;
     int x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.btn);
+        tv= findViewById(R.id.tv);
     }
     public void Click(View view){
         x ++;
+        tv.setWidth(10);
+        tv.setTextColor(Color.GREEN);
         btn.setText("This a click number:"+ x);
     }
 }
