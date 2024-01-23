6fdd80b327546e0f6472d222ce438c8633a567d7
imageview2 <ba6677@bs.amalnet.k12.il>
2024-01-23 09:13:19 +0200

    e

---
+++ app/src/main/java/com/example/botton1/Botton.java
@@ -0,0 +1,6 @@
+package com.example.botton1;
+
+public class Botton {
+    public void setText(String s) {
+    }
+}
---
+++ app/src/main/java/com/example/botton1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.botton1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    int x;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+    }
+    public void Click(View view){
+        x ++;
+        btn.setText("This a click number:"+ x);
+    }
+}
